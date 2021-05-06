4d440979042a14cf46d12e476c8908d2887d7233
LukaszKruszynski <lukaszkruszynski96@gmail.com>
2021-05-06 17:10:44 +0200

    builded order object in test

--- src/test/java/com/shop/FlashShopApplicationTests.java
+++ src/test/java/com/shop/FlashShopApplicationTests.java
@@ -1,13 +1,63 @@
 package com.shop;
 
+import com.shop.dto.order.OrderDto;
+import com.shop.model.order.*;
+import com.shop.model.product.Product;
+import com.shop.service.order.OrderService;
+import com.shop.service.product.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @SpringBootTest
 class FlashShopApplicationTests {
 
+    @Autowired
+    private OrderService orderService;
+    @Autowired
+    private ProductService productService;
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void createOrder() {
+        OrderDeliveryAddress address = OrderDeliveryAddress.builder()
+                .street("Poreby")
+                .city("Poreby")
+                .postCode("97-400")
+                .address("50A")
+                .build();
+
+        ShippingDetails dhl = ShippingDetails.builder()
+                .orderDeliveryAddress(address)
+                .deliverCompany("DHL")
+                .shipmentNumber("83743828")
+                .webLink("www.dhl.pl/sledz-paczke")
+                .build();
+        OrderCustomerDetails customer = OrderCustomerDetails.builder()
+                .mail("DobryMail@gmail.com")
+                .phone("796-654-321")
+                .lastName("Mistrzu")
+                .firstName("Marek")
+                .build();
+
+        OrderDetails orderDetails = OrderDetails.builder()
+                .shippingDetails(dhl)
+                .isPaid(true)
+                .isSend(true)
+                .orderCustomerDetails(customer)
+                .build();
+
+
+        Order order = Order.builder()
+                .products(productService.getProducts())
+                .orderDetails(orderDetails)
+                .totalAmount(productService.getProducts().stream().map(Product::getPrice).reduce(BigDecimal.ZERO, BigDecimal::add))
+                .build();
+
+    }
 }
